bf930eae6db1cc46ef03719c9633cfb9d1919055
Aki Karkkainen <akka@bownty.com>
2016-03-20 18:49:00 +0100

    Use separate CSS bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,45 @@
 var path = require('path');
 var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 require('es6-promise').polyfill();
 
 module.exports = {
-
   entry: './src/main.js',
 
   output: {
     path: __dirname,
-    filename: 'bundle.js'
+    filename: 'js/app.js'
   },
+
+  plugins: [
+    // Specify the resulting CSS filename
+    new ExtractTextPlugin('css/app.css')
+  ],
 
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass']}
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader!postcss!sass-loader?outputStyle=expanded'
+        )
+      }
     ]
   },
 
-  postcss: [autoprefixer({browsers: ['last 2 versions']})],
-
-  plugins: [],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
 
   stats: {
     // Colored output
